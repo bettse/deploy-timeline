b0d577224fc5574d2fb63c7f62e0c6d68640d295
Eric Betts <bettse@netlify.com>
2020-08-21 11:29:48 -0700

    Control carousel with slider

--- src/DeployTimeline.js
+++ src/DeployTimeline.js
@@ -10,6 +10,11 @@ import Col from 'react-bootstrap/Col'
 import Carousel from 'react-bootstrap/Carousel'
 import Spinner from 'react-bootstrap/Spinner'
 import Image from 'react-bootstrap/Image'
+import Form from 'react-bootstrap/Form'
+
+const DECIMAL = 10;
+
+const parseDecimal = (str) => parseInt(str, DECIMAL)
 
 export default class DeployTimeline extends Component {
   constructor(props, context) {
@@ -22,6 +27,7 @@ export default class DeployTimeline extends Component {
     this.state = {
       deploys,
       gif: null,
+      selectedScreenshot: 0,
     }
   }
 
@@ -56,9 +62,15 @@ export default class DeployTimeline extends Component {
     this.setState({deploys})
   }
 
+  timelineChange = (event) => {
+    const { target } = event;
+    const { value } = target
+    this.setState({selectedScreenshot: parseDecimal(value)})
+  }
+
   render() {
     const { site } = this.props
-    const { deploys, gif } = this.state
+    const { deploys, gif, selectedScreenshot } = this.state
     const { name } = site
     //{id, site_id, plan, ssl_plan, premium, claimed, name, custom_domain, domain_aliases, password, notification_email, url, admin_url, deploy_id, build_id, deploy_url, state, screenshot_url, created_at, updated_at, user_id, error_message, ssl, ssl_url, force_ssl, ssl_status, max_domain_aliases, build_settings, processing_settings, prerender, prerender_headers, deploy_hook, published_deploy, managed_dns, jwt_secret, jwt_roles_path, account_slug, account_name, account_type, capabilities, paid_individual_site_subscription, dns_zone_id, identity_instance_id, use_functions, parent_user_id, automatic_tls_provisioning, disabled, lifecycle_state, id_domain, use_lm, build_image, automatic_tls_provisioning_expired, analytics_instance_id, functions_region, functions_config, plugins}
 
@@ -67,6 +79,7 @@ export default class DeployTimeline extends Component {
         <Spinner animation="grow" variant="primary" />
       );
     }
+    const screenshots = deploys.map(d => d.screenshot_url).filter(url => url)
 
     return (
       <Container>
@@ -91,16 +104,33 @@ export default class DeployTimeline extends Component {
         </Row>
         <Row className="justify-content-center">
           <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto">
-            <Carousel fade indicators={false} interval={null} keyboard={false} slide={false} touch={false} wrap={false}>
-            {deploys.filter(d => d.screenshot_url).map((deploy, i) => {
-              const { screenshot_url } = deploy
+            <Carousel
+              fade
+              indicators={false}
+              interval={null}
+              keyboard={false}
+              slide={false}
+              touch={false}
+              wrap={false}
+              activeIndex={selectedScreenshot}
+            >
+            {screenshots.map((url, i) => {
               return (
               <Carousel.Item key={i}>
-                <Image src={screenshot_url} fluid rounded alt="Screenshot" />
+                <Image src={url} fluid rounded alt="Screenshot" />
               </Carousel.Item>
               )
             })}
             </Carousel>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto">
+            <Form>
+              <Form.Group controlId="timeline">
+                <Form.Control type="range" onChange={this.timelineChange} value={selectedScreenshot} min={0} max={screenshots.length} step={1} size="lg"/>
+              </Form.Group>
+            </Form>
           </Col>
         </Row>
       </Container>
